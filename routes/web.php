4d99647cb01126ace1206e35700aea1cdca200c8
vdmax94 <vdmax94@gmail.com>
2023-01-13 14:34:04 +0200

    Done Routing and Base Controller with Model

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ $router->add('^(?P<controller>[a-z]+)/?(?P<id>\d+)?/?(?P<action>[a-z]+)?$');
 //Router for controller/supertaxon/id/  (default action - index. Another don't require)
 //(for showing the all items of concrete taxon that belong to concrete supertaxon)
 // for example - classes/divisio/2 (show the all classes that belong to divisio which id = 2)
-$router->add('^(?P<controller>[a-z]+)/?(?P<supertaxon>[a-z]+)/?(?P<id>\d+)?$');
+$router->add('^(?P<controller>[a-z]+)/(?P<supertaxon>[a-z]+)/(?P<id>\d+)?$');
